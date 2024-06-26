191a75a9d730c3cf860f2215da4e3fca5cd3eb3a
lainecedes <elaine.parieh@gmail.com>
2024-06-26 12:28:08 +0200

    Final changes, DONE

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,12 +1,27 @@
- // Initialize Highlight.js after the DOM has loaded
     document.addEventListener('DOMContentLoaded', (event) => {
-        // Highlight all code blocks on the page
         hljs.highlightAll();
     });
 
+    document.addEventListener("DOMContentLoaded", function() {
+        // Twitter loader animation
+        setTimeout(function() {
+            var loaderContainer = document.querySelector('.loader-container');
+            loaderContainer.style.opacity = '0';
+            loaderContainer.style.visibility = 'hidden';
+        }, 1000);
 
 
-// Favorite
+        // Highlight inline code
+        document.querySelectorAll('code:not(pre code)').forEach(function(codeElement) {
+            if (!codeElement.classList.contains('inline-code')) {
+                codeElement.classList.add('inline-code');
+            }
+            hljs.highlightElement(codeElement);
+        });
+    });
+
+
+// Favorite button (just because)
     document.addEventListener('DOMContentLoaded', (event) => {
         document.querySelectorAll('.favorite-button').forEach(button => {
             button.addEventListener('click', () => {
